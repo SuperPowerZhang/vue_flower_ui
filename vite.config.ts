1c711ed5d20c9df5a484abac5cb42d73a1839046
superpower <2285546178@qq.com>
2020-12-06 23:34:13 +0800

    modify vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,8 @@ import fs from 'fs'
 import {baseParse} from '@vue/compiler-core'
 
 export default {
+    base:'./',
+    assetsDir:'assets',
     plugins: [md()],
     vueCustomBlockTransforms: {
         demo: (options) => {
